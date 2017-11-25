ec3eb9e62ccd413aa7cc57cbad078c016c791ef1
parsvaf <parsadino2@gmail.com>
2017-11-25 14:24:16 +0330

    new

---
+++ onItem.java
@@ -0,0 +1,14 @@
+package com.burs.parsa.myapplication;
+
+import android.content.Context;
+
+import com.burs.parsa.myapplication.module.news;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Parsa on 11/24/2017.
+ */
+public interface onItem {
+    public void onItemClick(int pos);
+}
